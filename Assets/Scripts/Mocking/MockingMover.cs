bca950c8cf5402f1ace3e53a39fa003d69e0d000
DESKTOP-BRTMTMV\Nathan <natemi1010@gmail.com>
2019-09-06 12:43:00 +0100

    Added Unit Testing With Mocks

---
+++ Assets/Scripts/Mocking/MockingMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using SOLID.DependencyInversion;
+
+namespace SOLID.Mocking
+{
+    public class MockingMover
+    {
+        private readonly float speed = 0f;
+        private readonly Transform transformToMove = null;
+        private readonly IMovementInputGetter inputGetter = null;
+
+        public MockingMover(float speed, Transform transformToMove, IMovementInputGetter inputGetter)
+        {
+            this.speed = speed;
+            this.transformToMove = transformToMove;
+            this.inputGetter = inputGetter;
+        }
+
+        public void Move()
+        {
+            Vector3 movement = new Vector3
+            {
+                x = inputGetter.Horizontal,
+                y = 0f,
+                z = inputGetter.Vertical,
+            }.normalized;
+
+            movement *= speed * Time.deltaTime;
+
+            transformToMove.Translate(movement);
+        }
+    }
+}
---
+++ Assets/Tests/MoverTests.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using NUnit.Framework;
+using SOLID.DependencyInversion;
+using SOLID.Mocking;
+using UnityEngine;
+
+namespace Tests
+{
+    public class MoverTests
+    {
+        [Test]
+        [TestCase(1f)]
+        [TestCase(61.7f)]
+        [TestCase(-2312)]
+        public void MovingTransformHorizontal_MovesCorrectly(float speed)
+        {
+            var transform = new GameObject().transform;
+            var inputGetterMock = new Mock<IMovementInputGetter>();
+            inputGetterMock.Setup(x => x.Horizontal).Returns(1f);
+
+            var mover = new MockingMover(speed, transform, inputGetterMock.Object);
+            mover.Move();
+
+            Assert.AreEqual(transform.position, new Vector3(Time.deltaTime * speed, 0f, 0f));
+        }
+
+        [Test]
+        [TestCase(1f)]
+        [TestCase(61.7f)]
+        [TestCase(-2312)]
+        public void MovingTransformVertical_MovesCorrectly(float speed)
+        {
+            var transform = new GameObject().transform;
+            var inputGetterMock = new Mock<IMovementInputGetter>();
+            inputGetterMock.Setup(x => x.Vertical).Returns(1f);
+
+            var mover = new MockingMover(speed, transform, inputGetterMock.Object);
+            mover.Move();
+
+            Assert.AreEqual(transform.position, new Vector3(0f, 0f, Time.deltaTime * speed));
+        }
+
+        [Test]
+        public void MovingTransformBothAxis_MovesCorrectly()
+        {
+            var transform = new GameObject().transform;
+            var inputGetterMock = new Mock<IMovementInputGetter>();
+            inputGetterMock.Setup(x => x.Horizontal).Returns(1f);
+            inputGetterMock.Setup(x => x.Vertical).Returns(-1f);
+
+            var mover = new MockingMover(1f, transform, inputGetterMock.Object);
+            mover.Move();
+
+            Assert.AreEqual(transform.position, new Vector3(1, 0f, -1).normalized * Time.deltaTime);
+        }
+    }
+}
